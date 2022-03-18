06c7fcadef3654ba956b3166a611d55e28395520
code-tanyasrivastava <tanya.1108srivastava@gmail.com>
2022-03-17 17:57:35 -0700

    [Sliding window] 1876. Substrings of Size Three with Distinct Characters

---
+++ 1876. Substrings of Size Three with Distinct Characters.py
@@ -0,0 +1,13 @@
+# 1876. Substrings of Size Three with Distinct Characters
+# A string is good if there are no repeated characters.
+# Given a string s, return the number of good substrings of length three in s.
+# Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+# A substring is a contiguous sequence of characters in a string.
+
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        count = 0
+        for i in range(len(s)-2):
+            if s[i]!= s[i+1] and s[i+1]!=s[i+2] and s[i]!=s[i+2]:
+                count+=1
+        return count
