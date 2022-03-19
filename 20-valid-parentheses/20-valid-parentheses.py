46185c3a899b11e2788e11d2caf3f3261a787384
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-03-19 15:30:53 -0700

    Time: 32 ms (88.29%), Space: 13.9 MB (37.49%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,7 +5,7 @@ class Solution:
         if len(s) <2:
             return False
         for k in range(len(s)):
-            if len(arr)>0: 
+            if arr: 
                 # to check if closing bracket and if openning bracket already used                                   
                 arr.pop(-1) if s[k] in d and arr[-1]==d[s[k]] else arr.append(s[k]) 
             else:
