dfbbbff19dc3801f7921619e36b9e9ed0c38887b
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-03-19 15:30:19 -0700

    Time: 33 ms (79.94%), Space: 13.6 MB (99.90%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,11 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         d = {')':'(','}':'{', ']':'['}
-        l =len(s)
         arr = []
-        if l <2:
+        if len(s) <2:
             return False
-        for k in range(l):
+        for k in range(len(s)):
             if len(arr)>0: 
                 # to check if closing bracket and if openning bracket already used                                   
                 arr.pop(-1) if s[k] in d and arr[-1]==d[s[k]] else arr.append(s[k]) 
