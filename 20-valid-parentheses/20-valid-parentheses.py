c0c1a48e781eb478b9d8ee5c889503c80eaa3537
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-03-19 15:37:26 -0700

    Time: 32 ms (88.29%), Space: 14 MB (37.49%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -7,7 +7,14 @@ class Solution:
         for k in range(len(s)):
             if arr: 
                 # to check if closing bracket and if openning bracket already used                                   
-                arr.pop(-1) if s[k] in d and arr[-1]==d[s[k]] else arr.append(s[k]) 
+                # arr.pop(-1) if s[k] in d and arr[-1]==d[s[k]] else arr.append(s[k])
+                if s[k] in d:
+                    if arr.pop(-1)==d[s[k]]:
+                        continue
+                    else:
+                        return False
+                else:
+                    arr.append(s[k])
             else:
                 # if openning bracket and if previous pairs found already and ths one is new combination                                  
                 arr.append(s[k])
