cb788bc512d2d8e4a75aa9f42d3e19feafceaadb
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-03-20 22:14:47 -0700

    Time: 212 ms (40.25%), Space: 16.8 MB (50.50%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,11 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # Method 2:
+        # Method 1:
         # c = Counter(nums)
         for i,v in Counter(nums).items():
             if v < 2:
                 return i
-        # Method 1
+        # Method 2:
         # nums.sort()
         # s = [nums[0]]
         # for i in range(1,len(nums)):
