61545a33116199f5f3073819c406969d1ee142b1
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-03-20 22:11:17 -0700

    Time: 162 ms (68.08%), Space: 17.2 MB (5.28%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,14 +1,23 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # Method 1
-        nums.sort()
-        s = [nums[0]]
-        for i in range(1,len(nums)):
-            if nums[i] != nums[i-1] :
-                s.append(nums[i])
-            else:
-                s.remove(nums[i])
-        return s[0]
+        # nums.sort()
+        # s = [nums[0]]
+        # for i in range(1,len(nums)):
+        #     if nums[i] != nums[i-1] :
+        #         s.append(nums[i])
+        #     else:
+        #         s.remove(nums[i])
+        # return s[0]
+        
+        # Method 2:
+        c = Counter(nums)
+        print(c)
+        for i,v in c.items():
+            if v < 2:
+                return i
+            
+                
             
             
         
