dd439f3cfa80ff3daf9ed160b13a50276c19146c
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-03-25 15:15:50 -0700

    Time: 92 ms (5.38%), Space: 14 MB (34.96%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,12 +1,18 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        # Method1
         if s == '':
-            if len(t) == 1:
-                return t[0]
-            else:
-                return t
+            return t[0]
         t=list(t)
         for i in s:
             if i in t:
                 t.pop(t.index(i))
         return t[0]
+        # Method 2:
+        # s = list(s)
+        # s.sort()
+        # t = list(t)
+        # t.sort()
+        # for i in t:
+        #     if i not in s:
+        #         return i
