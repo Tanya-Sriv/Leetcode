4914f55fbb5b3c6b74cbcceec6febc271891cc5b
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-03-26 11:36:48 -0700

    Time: 39 ms (66.91%), Space: 13.9 MB (12.76%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -3,7 +3,7 @@ class Solution:
         # Method 1: Hamming weight [Runtime:34 ms Memory:13.8 MB]
         count = 0
         while n != 0:
-            n &= n-1
+            n = n & (n-1)
             count +=1
         return count
         
