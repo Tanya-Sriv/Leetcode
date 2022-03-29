6ca928a090e917d12975cf6ec19c8e06892ac4a3
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-03-28 21:44:35 -0700

    Time: 71 ms (76.56%), Space: 15.7 MB (67.73%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -1,12 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         nums.sort()
-        # print(nums, len(nums))
-        if len(nums)<3:
+        l = len(nums)
+        if l<3:
             return nums[0]
-        for i in range(0,len(nums),3):
-            if i <= len(nums)-3:
-                print(nums[i],nums[i+1],nums[i+2])
+        for i in range(0,l,3):
+            if i <= l-3:
                 if nums[i] == nums[i+1] and nums[i+1] == nums[i+2]:
                     continue
                 else:
