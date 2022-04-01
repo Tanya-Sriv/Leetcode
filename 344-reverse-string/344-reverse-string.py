7b8a3505e6dd3a0e3772e9fb4e3462009897110b
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-03-31 17:41:18 -0700

    Time: 431 ms (5.65%), Space: 20.8 MB (8.38%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -4,8 +4,13 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         # Method 1:
-        temp = ''
-        for i in range(len(s)//2):
-            temp = s[i]
-            s[i] = s[-1*i -1]
-            s[-1*i -1] = temp
+        # temp = ''
+        # for i in range(len(s)//2):
+        #     temp = s[i]
+        #     s[i] = s[-1*i -1]
+        #     s[-1*i -1] = temp
+        d = {index: value for index, value in enumerate(s)}
+        l = len(s)
+        for i in range(l//2):
+            s[i] = d[l-i-1]
+            s[-1*i -1] = d[i]
