bf9f5331ec9c5b549cc51a47ee4c90106b7fca37
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-03-31 17:46:43 -0700

    Time: 212 ms (81.58%), Space: 18.5 MB (21.46%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -6,7 +6,5 @@ class Solution:
         # Method 1:
         temp = ''
         for i in range(len(s)//2):
-            temp = s[i]
-            s[i] = s[-1*i -1]
-            s[-1*i -1] = temp
+            s[i], s[-1*i -1] = s[-1*i -1], s[i]
         
