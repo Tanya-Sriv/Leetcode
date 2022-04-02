36bdb169a858474cbeda116ef5ee8b206d6695d6
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-04-01 18:42:23 -0700

    Time: 30 ms (90.61%), Space: 13.8 MB (58.78%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,22 +1,22 @@
 class Solution:
-    # def fib(self, n: int) -> int:
+    def fib(self, n: int) -> int:
         # Method 1:
-        # fib = [0,1] 
-        # if n <2:
-        #     return n
-        # for i in range(2,n+1):
-        #     fib.append(fib[i-1]+fib[i-2])
-        # return fib[-1]
+        fib = [0,1] 
+        if n <2:
+            return n
+        for i in range(2,n+1):
+            fib.append(fib[i-1]+fib[i-2])
+        return fib[-1]
         
         # Method 2: 
         # if n<2:
         #     return n
         # return self.fib(n-1)+self.fib(n-2)
         
-    # Method 3: caching
-    cache = {0:0,1:1}
-    def fib(self, n: int) -> int:
-        if n in self.cache:
-            return self.cache[n]
-        self.cache[n] = self.fib(n-1) + self.fib(n -2)
-        return self.cache[n]
+    # Method 3: caching O(n), O(n)
+    # cache = {0:0,1:1}
+    # def fib(self, n: int) -> int:
+    #     if n in self.cache:
+    #         return self.cache[n]
+    #     self.cache[n] = self.fib(n-1) + self.fib(n -2)
+    #     return self.cache[n]
