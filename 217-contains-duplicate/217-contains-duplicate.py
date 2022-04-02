b09c7e63491ab65ea561676e2aa0bdf27a5784bf
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-04-01 21:48:38 -0700

    Time: 644 ms (38.05%), Space: 25.9 MB (73.00%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,6 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # Method 1
+        
         n = Counter(nums)
         if max(n.values()) > 1:
             return True
