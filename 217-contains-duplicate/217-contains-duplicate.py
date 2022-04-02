54eef672aab36dafd53727860d1b6c5a421bc321
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-04-01 21:36:38 -0700

    Time: 666 ms (33.23%), Space: 26 MB (73.00%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,22 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # Method 2
+        n = Counter(nums)
+        if max(n.values()) > 1:
+            return True
+        return False
+    
+        # Method 1
+        # array sort
+        #iter through element- if element present -> dic[val] = not null return true else skip.
+        # nums.sort()
+        # log = {}
+        # for i in range(len(nums)-1):
+        #     if nums[i] == nums[i+1]:
+        #         return True
+        #     else:
+        #         continue
+        # return False
+        
+        
+        
