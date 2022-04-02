df060edd49b35697057a6e5a95f2fd98d8118886
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-04-01 18:40:30 -0700

    Time: 56 ms (39.89%), Space: 13.8 MB (58.78%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def fib(self, n: int) -> int:
+    # def fib(self, n: int) -> int:
         # Method 1:
         # fib = [0,1] 
         # if n <2:
@@ -9,6 +9,14 @@ class Solution:
         # return fib[-1]
         
         # Method 2: 
-        if n<2:
-            return n
-        return self.fib(n-1)+self.fib(n-2)
+        # if n<2:
+        #     return n
+        # return self.fib(n-1)+self.fib(n-2)
+        
+    # Method 3: caching
+    cache = {0:0,1:1}
+    def fib(self, n: int) -> int:
+        if n in self.cache:
+            return self.cache[n]
+        self.cache[n] = self.fib(n-1) + self.fib(n -2)
+        return self.cache[n]
