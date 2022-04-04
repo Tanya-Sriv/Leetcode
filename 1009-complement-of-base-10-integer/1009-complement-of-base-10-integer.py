d2e24f60239ffee0dc192d8b1896d9965b3fac15
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-04-04 01:25:19 -0700

    Time: 31 ms (86.45%), Space: 13.8 MB (96.96%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -1,7 +1,11 @@
 class Solution:
     def bitwiseComplement(self, n: int) -> int:
         # 101 111 => 010
-        n_binary = str(format(n,"b"))
-        mask = int("1"*len(n_binary),2)
+        # Method 2:
+        # n_binary = str(format(n,"b"))
+        # mask = int("1"*len(n_binary),2)
+        # return n^mask
+        # Method 1: Runtime: 30 ms	Memory:13.8 MB
+        l = len(bin(n))-2
+        mask = 2**l -1
         return n^mask
-        
