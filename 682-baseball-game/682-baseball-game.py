8fb2c078247f78b87676f6a74d40c1ee2c014919
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-04-11 00:11:31 -0700

    Time: 69 ms (31.41%), Space: 14 MB (95.42%) - LeetHub

--- 682-baseball-game/682-baseball-game.py
+++ 682-baseball-game/682-baseball-game.py
@@ -12,7 +12,7 @@ class Solution:
         #     else:
         #         l.append(int(i))
         # return sum(l)
-        # Method 2:
+        # Method 2 :
         ops[0] = int(ops[0])
         if len(ops) == 1:
             return ops[0]
