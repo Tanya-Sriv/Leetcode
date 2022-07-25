206e01d87bd699594b9c9d0f2bc2779fbf13bb05
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-07-24 23:25:37 -0700

    Time: 322 ms (8.52%), Space: 18.9 MB (25.29%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -5,14 +5,7 @@ class Solution:
         prev_start = intervals[0][0]
         prev_end = intervals[0][1]
         for i in range(1, len(intervals)):
-            # if intervals[i][0] > prev_end:
-            #     out.append([prev_start, prev_end])
-            #     prev_start = intervals[i][0]
-            #     prev_end = intervals[i][1]
-            if intervals[i][0] >= prev_start and intervals[i][0] <= prev_end:
-                prev_start = min(intervals[i][0], prev_start)
-                prev_end = max(intervals[i][1],prev_end)
-            elif intervals[i][0] <= prev_start:
+            if (intervals[i][0] >= prev_start and intervals[i][0] <= prev_end) or (intervals[i][0] <= prev_start):
                 prev_start = min(intervals[i][0], prev_start)
                 prev_end = max(intervals[i][1],prev_end)
             elif intervals[i][0] > prev_start or intervals[i][0] > prev_end:
