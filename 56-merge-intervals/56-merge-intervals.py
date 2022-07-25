e93ff3d1e3c6502dac3b392acca12758136001a2
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-07-24 23:53:35 -0700

    Time: 278 ms (22.13%), Space: 18.1 MB (51.40%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,19 +1,31 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key=lambda x:x[0])
-        out = []           
-        prev_start = intervals[0][0]
-        prev_end = intervals[0][1]
-        for i in range(1, len(intervals)):
-            if (intervals[i][0] >= prev_start and intervals[i][0] <= prev_end) or (intervals[i][0] <= prev_start):
-                prev_start = min(intervals[i][0], prev_start)
-                prev_end = max(intervals[i][1],prev_end)
-            elif intervals[i][0] > prev_start or intervals[i][0] > prev_end:
-                out.append([prev_start, prev_end])
-                prev_start = intervals[i][0]
-                prev_end = intervals[i][1]
-        out.append([prev_start, prev_end])    
-        return out
+        # method 1: O(n2)
+        # intervals = sorted(intervals, key=lambda x:x[0])
+        # out = []           
+        # prev_start = intervals[0][0]
+        # prev_end = intervals[0][1]
+        # for i in range(1, len(intervals)):
+        #     if (intervals[i][0] >= prev_start and intervals[i][0] <= prev_end) or (intervals[i][0] <= prev_start):
+        #         prev_start = min(intervals[i][0], prev_start)
+        #         prev_end = max(intervals[i][1],prev_end)
+        #     elif intervals[i][0] > prev_start or intervals[i][0] > prev_end:
+        #         out.append([prev_start, prev_end])
+        #         prev_start = intervals[i][0]
+        #         prev_end = intervals[i][1]
+        # out.append([prev_start, prev_end])    
+        # return out
+        
+        # method 2: O(nlogn)
+        result = []
+        intervals.sort(key = lambda x:x[0])
+        for i in intervals:
+            if not result or result[-1][1] < i[0]:
+                result.append(i)
+            else:
+                result[-1][1] = max(result[-1][1], i[1])
+        return result
+        
         
                     
                     
