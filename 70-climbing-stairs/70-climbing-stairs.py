ac3a038b5754a809e8f4199aa79fc2487873f999
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-08-05 23:52:54 -0700

    Time: 47 ms (48.92%), Space: 13.9 MB (57.10%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        memo = [-1]*(n+1)
+        memo[1] = 1
+        memo[2] = 2
+        for i in range(3,n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+        return memo[n]
+        
