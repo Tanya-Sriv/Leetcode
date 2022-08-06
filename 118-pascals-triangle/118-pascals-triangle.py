da8ddc2a225c575176a59481e08c64ef10b6f76e
Tanya Srivastava <99914066+Tanya-Sriv@users.noreply.github.com>
2022-08-05 23:12:35 -0700

    Time: 37 ms (81.59%), Space: 13.9 MB (17.64%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,15 +1,26 @@
 class Solution:
     def generate(self, n: int) -> List[List[int]]:
+        # Method 1
+        # pas = [[1]]
+        # temp = []
+        # for i in range(1, n):
+        #     prev_row = pas[-1]
+        #     for j in range(len(prev_row)+1):
+        #         if j == 0 or j == len(prev_row):
+        #             s = 1
+        #         else:
+        #             s = prev_row[j-1] + prev_row[j]
+        #         temp.append(s)
+        #     pas.append(temp)
+        #     temp = []
+        
+        # Method 2
         pas = [[1]]
-        temp = []
         for i in range(1, n):
             prev_row = pas[-1]
-            for j in range(len(prev_row)+1):
-                if j == 0 or j == len(prev_row):
-                    s = 1
-                else:
-                    s = prev_row[j-1] + prev_row[j]
-                temp.append(s)
+            temp = [1]*(len(prev_row)+1)
+            for j in range(len(prev_row)-1):
+                temp[j+1] = prev_row[j] + prev_row[j+1]
             pas.append(temp)
             temp = []
         return pas
